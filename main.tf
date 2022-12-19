45775431687994e45ff12b9f4cfa2a4776ccae8f
yaseenHashiCorp <85969516+yaseenHashiCorp@users.noreply.github.com>
2022-12-19 10:41:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1a"
 }
 
 resource "random_pet" "sg" {}
@@ -45,7 +45,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
